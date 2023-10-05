af500641406d82b707e29a133ed0da621850c08d
Hopki <edis.ersenov23@abv.bg>
2023-10-05 12:37:21 +0300

    Register user implemented

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,40 +1,80 @@
 import { Link } from 'react-router-dom';
-
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import * as authService from '../../services/authService'
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { setUserSession } = useContext(AuthContext);
+
+    const registerHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await authService.register(username, email, password);
+
+            if (response.ok) {
+                setUserSession(response);
+                navigate("/login");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
         <div className="Auth-form-container">
-      <form className="Auth-form">
-        <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign Up</h3>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email Address"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-          <p className="text-center mt-2">
-            Already have an <Link to="/login">account?</Link>
-          </p>
+            <form className="Auth-form" onSubmit={registerHandler}>
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign Up</h3>
+                    <div className="form-group mt-3">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Email Address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="text-center mt-2">
+                        Already have an <Link to="/login">account?</Link>
+                    </p>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-      )
+    )
 }
 
 export default Register;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,21 @@
 const baseUrl = "https://localhost:3000/api";
 
 export const login = async (email, password) => {
-  return await fetch(`${baseUrl}/Authentication/Login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  });
+    return await fetch(`${baseUrl}/Authentication/Login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+    });
+};
+
+export const register = async (username, email, password) => {
+    return await fetch(`${baseUrl}/Authentication/Register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password })
+    });
 };
